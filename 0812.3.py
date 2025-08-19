e133d85205f7ff62c2c304f5eeb6e75be610de81
Micropuppy <129589554+Micropuppy@users.noreply.github.com>
2025-08-19 23:11:52 +0800

    Add files via upload

---
+++ 0812.3.py
@@ -0,0 +1,10 @@
+#一个简单的程序
+""" print表示输出结果
+    input 表示获取输入值"""
+print("欢迎来到pythpn世界！")
+age = input("请输入你的年龄：")
+ages = int(age)
+if ages >=18:#进行条件判断
+    print("你有投票权")
+else:
+    print("你没有投票权")
